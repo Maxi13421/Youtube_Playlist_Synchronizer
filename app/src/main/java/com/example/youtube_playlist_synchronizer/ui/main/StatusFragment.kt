eccc37a055006e4ad3ea67b8a897944f343d69fd
Maxi13421 <magleiser@gmx.de>
2022-10-18 13:30:50 +0200

    x

--- app/src/main/java/com/example/youtube_playlist_synchronizer/ui/main/StatusFragment.kt
+++ app/src/main/java/com/example/youtube_playlist_synchronizer/ui/main/StatusFragment.kt
@@ -6,7 +6,10 @@ import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.youtube_playlist_synchronizer.R
+import com.example.youtube_playlist_synchronizer.rounded_box
 
 
 object StatusFragment : Fragment(R.layout.status){
@@ -14,6 +17,7 @@ object StatusFragment : Fragment(R.layout.status){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
 
     }
@@ -26,6 +30,17 @@ object StatusFragment : Fragment(R.layout.status){
         textView.id = R.id.test
         textView.setTextColor(Color.parseColor("#FFFFFFFF"))
         v.addView(textView)
+
+        val fm: FragmentManager? = fragmentManager
+        val fragmentTransaction: FragmentTransaction = fm!!.beginTransaction()
+
+        val f = rounded_box()
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<ExampleFragment>(R.id.fragment_container_view)
+            }
+        }
     }
 
     fun updatetextview(view: Activity, text: String){
