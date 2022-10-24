7763f8290edcd5626e30e492843f6c00b79a520c
Maxi13421 <magleiser@gmx.de>
2022-10-24 23:29:09 +0200

    x

--- app/src/main/java/com/example/youtube_playlist_synchronizer/MainActivity.kt
+++ app/src/main/java/com/example/youtube_playlist_synchronizer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.youtube_playlist_synchronizer
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -8,6 +9,7 @@ import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageButton
 import com.example.youtube_playlist_synchronizer.ui.main.SectionsPagerAdapter
 import com.example.youtube_playlist_synchronizer.databinding.ActivityMainBinding
 import com.example.youtube_playlist_synchronizer.ui.main.StatusFragment
@@ -22,6 +24,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val setbtn: ImageButton = binding.setbtn
+        setbtn.setOnClickListener{ view ->
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = binding.viewPager
         viewPager.adapter = sectionsPagerAdapter
@@ -33,4 +41,6 @@ class MainActivity : AppCompatActivity() {
             StatusFragment.updatetextview(this, "new")
         }
     }
+
+
 }
